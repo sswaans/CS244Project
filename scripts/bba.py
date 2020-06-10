6e22e75cc0d9206ceab6d29da193411c56a56adf
stefanswaans <sswaans@stanford.edu>
2020-06-09 22:07:30 -0400

    Added more comprehensive tests, began fixing bug related to safe vs risky ratemap. May need to rework buffer/reservoir/cushion to use seconds rather than bytes...

--- scripts/bba.py
+++ scripts/bba.py
@@ -1,17 +1,20 @@
 from queue import Queue
+import random
 
 class BBASim:
 	def __init__(self, rates, chunkSec, bufSize, reservoirSize, cushionSize, capacity):
 		self.rates = rates
-		self.chunkSec = chunkSec
+		self.chunkSec = chunkSec # Number of seconds in a chunk
 		self.bufSize = bufSize
-		self.reservoirSize = reservoirSize # How many bytes of video we should always have (use min rate if below)
+		self.reservoirSize = min(reservoirSize, chunkSec * rates[0]) # How many bytes of video we should always have (use min rate if below)
 		self.cushionSize = cushionSize # How many bytes of video we should have before hitting max rate
 		self.capacity = capacity # Network capacity, C (bytes down)
 		self.buffer = 0
 		self.rate = rates[0]
 		self.rateQueue = Queue()
 		self.partialChunkBytes = 0 # Number of bytes we've already downloaded of current chunk
+		self.initialBufferComplete = False # Whether or not we have buffered the very first chunk of video
+		self.log = ""
 
 	def __rateMap(self):
 		if self.buffer <= self.reservoirSize:
@@ -23,11 +26,11 @@ class BBASim:
 			return self.rates[0] + (percentCushion * (self.rates[-1] - self.rates[0]))
 
 	def __getNextRate(self):
-		print("Previous rate: %s" % self.rate)
+		self.log += "Previous rate: " + str(self.rate) + "\n"
 		ratePlus = self.rates[-1] if self.rate == self.rates[-1] else min([rate for rate in self.rates if rate > self.rate])
 		rateMinus = self.rates[0] if self.rate == self.rates[0] else max([rate for rate in self.rates if rate < self.rate])
 		rateSuggest = self.__rateMap()
-		print("Suggested rate: %s" % rateSuggest)
+		self.log += "Suggested rate: " + str(rateSuggest) + "\n"
 		rateNext = self.rate
 		if rateSuggest == self.rates[0] or rateSuggest == self.rates[-1]:
 			rateNext = rateSuggest
@@ -35,62 +38,158 @@ class BBASim:
 			rateNext = max([rate for rate in self.rates if rate < rateSuggest])
 		elif rateSuggest <= rateMinus:
 			rateNext = min([rate for rate in self.rates if rate > rateSuggest])
-		print("New rate: %s" % rateNext)
+		# Have to pick a "safe" rate, not "risky" (i.e. chunk must finish before buffer runs below reservoir)
+		# TODO: This may not be entirely accurate, RHS is # secs buffered - # reservoir secs (but at min rate, could be problem)
+		if self.buffer > self.reservoirSize and rateNext * self.chunkSec / rates[0] > self.rateQueue.qsize() - (self.reservoirSize / rates[0]):
+			availableRates = [rate for rate in self.rates if rate * self.chunkSec / rates[0] <= self.rateQueue.qsize() - (self.reservoirSize / rates[0])]
+			if not availableRates:
+				return -2
+			rateNext = max(availableRates)
+		# Custom addition: never return a (rate * chunk sec) greater than buffer size
+		if rateNext * self.chunkSec > self.bufSize:
+			availableRates = [rate for rate in self.rates if rate * self.chunkSec <= self.bufSize]
+			if not availableRates:
+				return -1
+			rateNext = max(availableRates)
+		self.log += "New rate: " + str(rateNext) + "\n"
 		return rateNext
 
-	def simulateSecond(self):
-		# TODO: Does it matter that we do all of our downloads before all of our drain?
-		# TODO: Incorporate chunk sec (assuming 1 sec chunks right now)
+	def simulateSecond(self, capacity=None):
+		# TODO: Currently only support integer chunkSec >= 1. Adding support for floats is nontrivial.
+		# -----DRAIN-----
+		self.log += "DRAIN\n"
+		if self.initialBufferComplete:
+			if self.rateQueue.empty():
+				self.log += "NO CHUNK FULLY DOWNLOADED!\n"
+				self.log += "bufSize: " + str(self.bufSize) + "\n"
+				self.log += "chunkSec: " + str(self.chunkSec) + "\n"
+				self.log += "cushionFrac: " + str(self.cushionSize / self.bufSize) + "\n"
+				self.log += "capacity: " + str(self.capacity) + "\n"
+				self.log += "reservoir: " + str(self.reservoirSize) + "\n"
+				self.log += "\n\n\n\n\n\n\n\n"
+				print(self.log)
+				return False
+			drainRate = self.rateQueue.get(block=False)
+			self.buffer -= drainRate
+			if self.buffer < 0:
+				self.log += "BUFFER RAN EMPTY!\n"
+				self.log += "bufSize: " + str(self.bufSize) + "\n"
+				self.log += "chunkSec: " + str(self.chunkSec) + "\n"
+				self.log += "cushionFrac: " + str(self.cushionSize / self.bufSize) + "\n"
+				self.log += "capacity: " + str(self.capacity) + "\n"
+				self.log += "reservoir: " + str(self.reservoirSize) + "\n"
+				self.log += "\n\n\n\n\n\n\n\n"
+				print(self.log)
+				return False
+			self.log += "Drained rate: " + str(drainRate) + "\n"
+		self.log += "Approx blocks in queue: " + str(self.rateQueue.qsize()) + "\n"
+		self.log += "=======================================\n"
+
 		# ----DOWNLOAD-----
-		print("DOWNLOAD")
+		self.log += "DOWNLOAD\n"
+		if capacity:
+			self.capacity = capacity
 		if self.partialChunkBytes == 0:
-			self.rate = self.__getNextRate()
+			newRate = self.__getNextRate()
+			if newRate < 0:
+				self.log += "BUFFER TOO SMALL, NO SUITABLE RATE.\n" if newRate == -1 else "SAFE RATE NOT AVAILABLE.\n"
+				self.log += "bufSize: " + str(self.bufSize) + "\n"
+				self.log += "chunkSec: " + str(self.chunkSec) + "\n"
+				self.log += "cushionFrac: " + str(self.cushionSize / self.bufSize) + "\n"
+				self.log += "capacity: " + str(self.capacity) + "\n"
+				self.log += "reservoir: " + str(self.reservoirSize) + "\n"
+				self.log += "\n\n\n\n\n\n\n\n"
+				print(self.log)
+				return False
+			self.rate = newRate
 		bufRemaining = self.bufSize - self.buffer
-		print("bufRemaining: %s" % bufRemaining)
+		self.log += "Buffer remaining: " + str(bufRemaining) + "\n"
 		if bufRemaining > 0:
 			capacityRemaining = self.capacity
-			chunkRemaining = self.rate - self.partialChunkBytes
-			print("Capacity remaining: %s" % capacityRemaining)
-			print("Chunk remaining: %s" % chunkRemaining)
+			chunkRemaining = self.rate * self.chunkSec - self.partialChunkBytes
+			self.log += "Capacity remaining: " + str(capacityRemaining) + "\n"
+			self.log += "Chunk remaining: " + str(chunkRemaining) + "\n"
 			# If we can, download a full single chunk and reevaluate rate
 			while bufRemaining >= chunkRemaining and chunkRemaining <= capacityRemaining:
-				print("Finishing chunk")
+				self.log += "Finishing chunk\n"
 				self.buffer += chunkRemaining
 				capacityRemaining -= chunkRemaining
 				bufRemaining -= chunkRemaining
-				self.rateQueue.put(self.rate)
-				self.rate = self.__getNextRate()
-				chunkRemaining = self.rate
+				for _ in range(self.chunkSec):
+					self.rateQueue.put(self.rate)
+				self.initialBufferComplete = True
+				if min(capacityRemaining, bufRemaining) > 0:
+					newRate = self.__getNextRate()
+					if newRate < 0:
+						self.log += "BUFFER TOO SMALL, NO SUITABLE RATE.\n" if newRate == -1 else "SAFE RATE NOT AVAILABLE.\n"
+						self.log += "bufSize: " + str(self.bufSize) + "\n"
+						self.log += "chunkSec: " + str(self.chunkSec) + "\n"
+						self.log += "cushionFrac: " + str(self.cushionSize / self.bufSize) + "\n"
+						self.log += "capacity: " + str(self.capacity) + "\n"
+						self.log += "reservoir: " + str(self.reservoirSize) + "\n"
+						self.log += "\n\n\n\n\n\n\n\n"
+						print(self.log)
+						return False
+					self.rate = newRate
+					chunkRemaining = self.rate * self.chunkSec
 				self.partialChunkBytes = 0
-				print("----------------------")
-				print("bufRemaining: %s" % bufRemaining)
-				print("Capacity remaining: %s" % capacityRemaining)
-				print("Chunk remaining: %s" % chunkRemaining)
+				self.log += "----------------------\n"
+				self.log += "Buffer remaining: " + str(bufRemaining) + "\n"
+				self.log += "Capacity remaining: " + str(capacityRemaining) + "\n"
+				self.log += "Chunk remaining: " + str(chunkRemaining) + "\n"
 			# If we can't download a full single chunk, download as much as capacity and
 			# remaining buffer allow and note how much of the chunk we downloaded
 			self.buffer += min(capacityRemaining, bufRemaining)
 			self.partialChunkBytes += min(capacityRemaining, bufRemaining)
-			print("Couldn't finish chunk, downloaded %s" % self.partialChunkBytes)
+			self.log += "Couldn't finish chunk, downloaded " + str(self.partialChunkBytes) + "\n"
 		else:
-			print("Buffer full, no download this cycle")
+			self.log += "Buffer full, no download this cycle\n"
 
-		print("============================")
-		# -----DRAIN-----
-		print("DRAIN")
-		self.buffer -= self.rateQueue.get()
-		if self.buffer < 0:
-			print("BUFFER RAN EMPTY!")
-			exit()
-		print("Drained buffer: %s" % self.buffer)
-		print("Approx blocks in queue: %s" % self.rateQueue.qsize())
-		print("=======================================")
-		print("=======================================")
+		self.log += "============================\n"
+		self.log += "============================\n"
+		return True
+
+	def printLog(self):
+		self.log += "bufSize: " + str(self.bufSize) + "\n"
+		self.log += "chunkSec: " + str(self.chunkSec) + "\n"
+		self.log += "cushionFrac: " + str(self.cushionSize / self.bufSize) + "\n"
+		self.log += "capacity: " + str(self.capacity) + "\n"
+		self.log += "reservoir: " + str(self.reservoirSize) + "\n"
+		self.log += "\n\n\n\n\n\n\n\n"
+		print(self.log)
 
 
 if __name__ == "__main__":
 	rates = [400, 800, 1200, 1500, 4000, 8000]
-	bufSize = 96000
-	bbaSim = BBASim(rates, 4, 96000, 1 * rates[0], 0.9 * bufSize, 7000)
+	bufSizes = [100, 400, 2000, 10000, 40000, 96000]
+	chunkSecs = [1, 2, 3, 5, 10]
+	cushionFracs = [0.25, 0.5, 0.75, 0.9, 1.0]
+	capacities = [400, 2000, 3000, 7000, 16000]
+	reservoirSizes = [400, 1000, 2000, 4000, 8000]
+	# Test with fixed capacities
 	ratePrev = rates[0]
-	for i in range(1000):
-		rateNext = bbaSim.simulateSecond()
+	for bufSize in bufSizes:
+		for chunkSec in chunkSecs:
+			for cushionFrac in cushionFracs:
+				for capacity in capacities:
+					for reservoirSize in reservoirSizes:
+						bbaSim = BBASim(rates, chunkSec, bufSize, reservoirSize, cushionFrac * bufSize, capacity)
+						for i in range(100):
+							success = bbaSim.simulateSecond()
+							if not success:
+								break
+						#bbaSim.printLog()
+						
+	# Test with random capacities
+	# for bufSize in bufSizes:
+	# 	for chunkSec in chunkSecs:
+	# 		for cushionFrac in cushionFracs:
+	# 			for reservoirSize in reservoirSizes:
+	# 				capacity = random.choice(capacities)
+	# 				bbaSim = BBASim(rates, chunkSec, bufSize, reservoirSize, cushionFrac * bufSize, capacity)
+	# 				for i in range(10):
+	# 					capacity = random.choice(capacities)
+	# 					success = bbaSim.simulateSecond(capacity)
+	# 					if not success:
+	# 						break
+	# 				#bbaSim.printLog()
